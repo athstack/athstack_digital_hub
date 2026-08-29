2f73fc890f510791f852bf231e40e3633ec759d1
U77Y <your-email@example.com>
2026-08-29 21:37:05 +0300

    cleanup: remove debug endpoint

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,5 @@ const shopController = require('../controllers/shopController');
 
 router.get('/', shopController.getShop);
 router.get('/:slug', shopController.getProduct);
-router.get('/debug/reviews', shopController.debugReviews);
 
 module.exports = router;
